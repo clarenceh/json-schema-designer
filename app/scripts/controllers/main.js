8b3300a567f6eeaa60df760f360d31b1feeb55f6
Clarence Ho <clarence@skywidesoft.com>
2013-10-05 10:32:17 +0800

    Add modal dialog for add new schema object

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,15 @@ angular.module('jsonSchemaDesignerApp')
         ];
 
         $scope.jsonObjArray = [];
+        $scope.objName = '';
+        var dblClickPos = {posX: 0, posY: 0};
+
+        // Modal dialog for add new JSON schema object
+        var addSchemaObjModal = $('#addSchemaObjModal');
+        addSchemaObjModal.on('hidden.bs.modal', function () {
+            $log.info('Add schema obj dialog closed');
+            //$log.info('Obj name: ' + $scope.objName);
+        });
 
         var canvas = new fabric.Canvas('c');
 
@@ -20,40 +29,55 @@ angular.module('jsonSchemaDesignerApp')
             var canvasPosition = canvasArea.offset();
             $log.info('Canvas position is at: ' + canvasPosition.left + ', ' + canvasPosition.top);
 
-            var clickPosX = $event.pageX - canvasPosition.left;
-            var clickPosY = $event.pageY - canvasPosition.top;
-            $log.info('Clicked Canvas position is at: ' + clickPosX + ', ' + clickPosY);
+            //var clickPosX = $event.pageX - canvasPosition.left;
+            //var clickPosY = $event.pageY - canvasPosition.top;
+            dblClickPos.posX = $event.pageX - canvasPosition.left;
+            dblClickPos.posY = $event.pageY - canvasPosition.top;
+            $log.info('Clicked Canvas position is at: ' + dblClickPos.posX + ', ' + dblClickPos.posY);
 
-            // Create a new instance of JsonSchemaObj
-            var jsonSchemaObj = new JsonSchemaObj();
-            jsonSchemaObj.name = 'Testing2';
+            // Display modal
+            addSchemaObjModal.modal('show');
 
-            // Define a text
-            var text = new fabric.Text(jsonSchemaObj.name, {
-                fontSize: 10
-            }) ;
-
-            // Define a rectangle
-            var rect = new fabric.Rect({
-                fill: '#eef',
-                width: 150,
-                height: 30
-            });
-
-            var group = new fabric.Group([rect, text], {
-                left: clickPosX,
-                top: clickPosY
-            });
-
-            //jsonSchemaObj.group = group;
-            group.jsonSchemaObj = jsonSchemaObj;
-
-            // Push the new object into the object array
-            $scope.jsonObjArray.push(jsonSchemaObj);
-
-            $log.info('No of objects: ' + $scope.jsonObjArray.length);
-            canvas.add(group);
         }
+
+        $scope.addSchemaObjModalOk = function() {
+            $log.info('Obj name: ' + $scope.objName);
+            addSchemaObjModal.modal('hide');
+
+            if ($scope.objName !== '') {
+                // Create a new instance of JsonSchemaObj
+                var jsonSchemaObj = new JsonSchemaObj();
+                jsonSchemaObj.name = $scope.objName;
+
+                // Define a text
+                var text = new fabric.Text(jsonSchemaObj.name, {
+                    fontSize: 10
+                }) ;
+
+                // Define a rectangle
+                var rect = new fabric.Rect({
+                    fill: '#eef',
+                    width: 150,
+                    height: 30
+                });
+
+                var group = new fabric.Group([rect, text], {
+                    left: dblClickPos.posX,
+                    top: dblClickPos.posY
+                });
+
+                //jsonSchemaObj.group = group;
+                group.jsonSchemaObj = jsonSchemaObj;
+
+                // Push the new object into the object array
+                $scope.jsonObjArray.push(jsonSchemaObj);
+
+                $log.info('No of objects: ' + $scope.jsonObjArray.length);
+                canvas.add(group);
+
+                $scope.objName = '';
+            }
+        };
 
         canvas.on('object:selected', function(options){
 
